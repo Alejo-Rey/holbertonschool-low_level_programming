b4420a1257320876eba6b52358d75138aa229fcb
Alejo-Rey <771@holbertonschool.com>
2019-02-14 23:16:12 +0000

    print 14 digits

---
+++ 0x03-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - check the code for Holberton School students.
+ *
+ *
+ * Return: Always 0.
+ */
+void more_numbers(void)
+{
+	int count;
+	int count2;
+
+	for (count = 1; count <= 10; count++)
+	
+	{
+		for (count2 = 0; count2 <= 14; count2++)
+		{
+			if(count2 > 10)
+			{
+				_putchar(count2 / 10 + '0'); 
+			}	
+			_putchar(count2 % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
