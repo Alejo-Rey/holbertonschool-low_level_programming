c12600c809b4160163ed22a2900e5cdbad32a9e2
Alejo-Rey <771@holbertonschool.com>
2019-02-15 01:01:44 +0000

    print / in diagonal

---
+++ 0x03-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - check the code for Holberton School students.
+ *
+ * @n: number of the diagonals
+ * Return: Always 0.
+ */
+void print_diagonal(int n)
+{
+	int count;
+	int count2;
+
+		for (count = 0; count < n; count++)
+		{
+			for (count2 = 0; count2 < count; count2++)
+			{	
+				_putchar('.');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+}
