dc63a3b182fa882bad8e94a199ebd6233262f11d
Alejo-Rey <771@holbertonschool.com>
2019-02-18 23:56:12 +0000

    dasaf

---
+++ 0x04-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * _strlen - change pointers.
+ *@s: value to change
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	char *count;
+
+	for (; count <= s; count++)
+	       return (count);
+}
