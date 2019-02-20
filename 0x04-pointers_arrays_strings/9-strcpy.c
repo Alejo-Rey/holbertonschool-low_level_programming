dfba6021adf37517d87d417ac5325f6a690a6504
Alejo-Rey <771@holbertonschool.com>
2019-02-20 22:54:12 +0000

    pass one array to another

---
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+  * _strcpy - print elements in another array
+  *
+  * @dest: array destine
+  * @src: array origin
+  * retunr 0.
+  */
+
+char *_strcpy(char *dest, char *src)
+{
+	int count;
+
+	for (count = 0; src[count]; count++)
+	{
+		dest[count] = src[count];
+	}
+	dest[count] = '\0';
+
+	return (dest);
+}
