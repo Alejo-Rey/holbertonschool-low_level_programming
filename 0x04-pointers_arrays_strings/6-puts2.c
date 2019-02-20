a5a0131967fc0de1ef9ef55d6e6c643aef54c230
Alejo-Rey <771@holbertonschool.com>
2019-02-20 18:33:05 +0000

    put array in pars

---
+++ 0x04-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * puts2  -  change pointers.
+ * @str: value of int
+ *
+ * Return: Always 0.
+ */
+void puts2(char *str)
+{
+	int count;
+
+	for (count = 0; str[count] != '\0'; count++)
+	{
+		if (str[count] % 2 == 0)
+			_putchar(str[count]);
+	}
+	_putchar('\n');
+}
