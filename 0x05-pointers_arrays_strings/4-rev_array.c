530551b9ada574ac76b9c7ca136a19a6ed208786
Alejo-Rey <771@holbertonschool.com>
2019-02-22 22:05:50 +0000

    first tried

---
+++ 0x05-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * reverse_array -  change pointers.
+ * @a: value of int
+ * @n: number of characters
+ * Return: Always 0.
+ */
+void reverse_array(int *a, int n)
+{
+	int count, rev;
+
+	for (count = 0; count < n && a[count] != '\0'; count++)
+	{
+	}
+	for (; count  >= 0; count--)
+		a[count];
+}
