6507e3d89a27ac88ba0b7f2d0f8d0fe3b5818227
Alejo-Rey <771@holbertonschool.com>
2019-02-22 00:00:10 +0000

    appends array

---
+++ 0x05-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * _strcat - appends one array to another.
+ * @dest: array 1
+ * @src: array that is append
+ * Return: Always 0.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int len, count;
+
+	for (len = 0; src[len]; len++)
+	{}
+
+	for (count = 0; src[count]; count++)
+	{
+		dest[len + count] = src[count];
+	}
+
+	return (dest);
+}
