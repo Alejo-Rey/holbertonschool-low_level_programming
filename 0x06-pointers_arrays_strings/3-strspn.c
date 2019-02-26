e6075dc0c0a52652ff7d345b8105e9991b9ab623
Alejo-Rey <771@holbertonschool.com>
2019-02-26 01:47:30 +0000

    man strspn calculates the length of the initial segment of str1 which consists entirely of characters in str

--- 0x06-pointers_arrays_strings/3-strspn.c
+++ 0x06-pointers_arrays_strings/3-strspn.c
@@ -9,5 +9,20 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
+	unsigned int count, i, j;
 
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				count += 1;
+			}
+
+		}
+		if (count == i)
+			break;
+	}
+	return (count);
 }
