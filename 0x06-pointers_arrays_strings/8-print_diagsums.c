8e58b14c6d21b36bcdc37b3886f59eb2afe4cdb5
Alejo-Rey <771@holbertonschool.com>
2019-02-26 19:22:23 +0000

    sum a diagonals of a double array

--- 0x06-pointers_arrays_strings/8-print_diagsums.c
+++ 0x06-pointers_arrays_strings/8-print_diagsums.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
  * set_string - prints buffer in hexa
@@ -9,5 +10,16 @@
 
 void set_string(char **s, char *to)
 {
+	int count, count2;
+	/*int size = sizeof(s)/sizeof(char);*/
 
+	for (count = 0; s[count] != '\0'; count++)
+	{
+		for (count2 = 0; to[count] != '\0'; count2)
+		{
+			if (count == count2)
+				printf("%d, ", s[count] + to[count2]);
+		}
+		printf("%d", s[count] + (to[count2] - count));
+	}
 }
