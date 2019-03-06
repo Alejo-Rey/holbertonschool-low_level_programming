bc838a5f5b41725213522b2afe2b4f398ff6a43a
Alejo-Rey <771@holbertonschool.com>
2019-03-06 00:02:09 +0000

    allocate a pointer to another

--- 0x0A-malloc_free/1-strdup.c
+++ 0x0A-malloc_free/1-strdup.c
@@ -1,11 +1,23 @@
 #include "holberton.h"
 
 /**
- * _strdup -
- *
- * Return: 
+ * _strdup - allocate a pointer in other
+ * @str:the pointer to allocate
+ * Return: return the new pointer
  */
 char *_strdup(char *str)
 {
+	int count, count2;
+	char *x;
 
+	if (str == NULL)
+		return (NULL);	
+	for (count = 0; str[count]; count++)
+	{}
+	x = malloc(sizeof(char) * count);
+	if (x == NULL)
+		return (NULL);
+	for (count2 = 0; str[count2]; count2++)
+		x[count2] = str[count2];
+	return (x);
 }
