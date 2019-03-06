6a6d20c4c9bade7e621d3493e5038135df5ad20a
Alejo-Rey <771@holbertonschool.com>
2019-03-06 00:50:44 +0000

    Allocate to a pointer and concatenate

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -2,10 +2,30 @@
 
 /**
  * str_concat -
- *
+ * @
  * Return: 
  */
 char *str_concat(char *s1, char *s2)
 {
+	int a = 0, b = 0, c = 0, d = 0, e;
+	char *x;
+	
+	for ( ; s1[a]; a++)
+	{}
+	for ( ; s2[b]; b++)
+	{}
+	e = a + b;
+	x = malloc(sizeof(char) * e);
 
+	for ( ; c < a; c++)
+		x[c] = s1[c];
+	for ( ; d < b; d++)
+	{
+		x[c] = s2[d];
+		c++;
+	}
+	if (x == NULL)
+		return (NULL);
+
+	return (x);
 }
