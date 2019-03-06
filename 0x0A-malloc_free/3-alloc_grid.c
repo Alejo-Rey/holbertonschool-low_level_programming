c6c1a5b0269f500c58924ee3f0181d05fa20aa99
Alejo-Rey <771@holbertonschool.com>
2019-03-06 04:52:45 +0000

    malloc, free

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -1,11 +1,21 @@
 #include "holberton.h"
 
 /**
- * alloc_grid -
- *
- * Return: 
+ * alloc_grid - square
+ * @width: line x
+ * @height: line y
+ * Return: always 0.
  */
 int **alloc_grid(int width, int height)
 {
+	int a = 0, b = 0;
 
+	for ( ; b <= height, b++)
+	{
+		for ( ; a <= width; a++)
+		{
+
+		}
+	}
+	return (0);
 }
--- 0x0A-malloc_free/4-free_grid.c
+++ 0x0A-malloc_free/4-free_grid.c
@@ -1,11 +1,15 @@
 #include "holberton.h"
 
 /**
- * free_grid -
- *
- * Return: 
+ * free_grid - qwedd asd
+ * @grid: grid square
+ * @height: height size
+ * Return: alway 0
  */
 void free_grid(int **grid, int height)
 {
+	int i;
 
+	for (i = 0; i < height; i++)
+		;
 }
