6534191bb32f60adc3f966f27923a9f25453be24
Alejo-Rey <771@holbertonschool.com>
2019-03-08 23:25:08 +0000

    using calloc with malloc, set \0

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -1,11 +1,25 @@
 #include "holberton.h"
 
 /**
- * _calloc -
- *
- * Return: 
+ * _calloc - funtion to use calloc
+ * @nmemb: size int;
+ * @size: size of type
+ * Return: always something, maybe
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	unsigned int a;
+	char *x;
 
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	x = malloc(nmemb + size);
+	if (x == NULL)
+		return (NULL);
+
+	for (a = 0; a <= nmemb; a++)
+		x[a] = '\0';
+
+	return (x);
 }
