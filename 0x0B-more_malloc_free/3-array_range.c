391788f216a512960272d7270da8bbe7e5788fe0
Alejo-Rey <771@holbertonschool.com>
2019-03-09 00:35:10 +0000

    array of min and max

--- 0x0B-more_malloc_free/3-array_range.c
+++ 0x0B-more_malloc_free/3-array_range.c
@@ -1,11 +1,27 @@
 #include "holberton.h"
-
+#include <stdio.h>
 /**
- * array_range -
- *
- * Return: 
+ * array_range - array of min to max
+ * @min: minumun characters
+ * @max: characters
+ * Return: something always
  */
 int *array_range(int min, int max)
 {
+	int a, *x;
 
+	if (min > max)
+		return (NULL);
+	x = malloc(sizeof(int) * (max - min + 1));
+	if (x == NULL)
+		return (NULL);
+
+	/*printf("min: %i, max: %i\n", min, max);*/
+	for (a = 0; a < (max - min + 1); a++)
+	{
+		x[a] = min;
+		min++;
+	}
+	/*printf("min: %i, max: %i\n", min, max);*/
+	return (x);
 }
