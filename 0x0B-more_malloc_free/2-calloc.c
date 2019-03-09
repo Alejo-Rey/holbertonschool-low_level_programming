50d241dcdc2636cb00bcdffe7b9c448b5c5ce7de
Alejo-Rey <771@holbertonschool.com>
2019-03-09 00:00:14 +0000

    using calloc with malloc, set \0

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -14,7 +14,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	x = malloc(nmemb + size);
+	x = malloc(nmemb * size);
 	if (x == NULL)
 		return (NULL);
 
