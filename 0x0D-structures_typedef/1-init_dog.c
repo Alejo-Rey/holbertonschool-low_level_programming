2c9f30dfd8de675cee2edf0a47f8619b3eb174ea
Alejo-Rey <771@holbertonschool.com>
2019-03-12 21:05:33 +0000

    write a funtion to initialize a struct

--- 0x0D-structures_typedef/1-init_dog.c
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -5,7 +5,12 @@
  *
  * Return: 
  */
-void print_dog(struct dog *d)
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-
+	if (d != NULL)
+	{
+		(*d).name = name;
+		(*d).age = age;
+		(*d).owner = owner;
+	}
 }
