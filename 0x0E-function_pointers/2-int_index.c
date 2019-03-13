9aabeb8464f73b3053caf8130c3b12a99ce62043
Alejo-Rey <771@holbertonschool.com>
2019-03-13 22:29:37 +0000

    pointer to a function that find a integer in its position

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -1,11 +1,26 @@
 #include "function_pointers.h"
 
 /**
- * int_index -
- *
- * Return: 
+ * int_index - function to find a int
+ * @array: array of the integers
+ * @size: size of the integer
+ * @cmp: function of conditional
+ * Return: return x for position or -1 if this fail
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
+	int x;
 
+	if (size <= 0)
+		return (-1);
+	if (array != 0 && cmp != 0)
+	{
+		for (x = 0; x < size; x++)
+		{
+			if (cmp(array[x]) == 1)
+				return (x);
+
+		}
+	}
+	return (-1);
 }
