a82002d53eeea6b899e8d87b94f0b5cb05b8df4f
Alejo-Rey <771@holbertonschool.com>
2019-03-13 19:37:46 +0000

    asigne the name in the pointer to function

---
+++ 0x0E-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - pointer to function for print a name
+ * @name: name called in the function
+ * @f: pointer of the function
+ * Return: maybe
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL)
+		f(name);
+}
