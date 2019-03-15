dc242906f7535accff6865065337da56d8d7b803
Alejo-Rey <771@holbertonschool.com>
2019-03-15 23:18:25 +0000

    [feature] create a variadic funtion to print n strings

---
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -0,0 +1,34 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - funtion to print variadic funtion od strings
+ * @separator: separator of the strings
+ * @n: number of string
+ * Return: no return
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list string;
+	unsigned int x;
+	char *p;
+
+	va_start(string, n);
+
+	if (separator != NULL && n != 0)
+	{
+		for (x = 0; x < n; x++)
+		{
+			p = va_arg(string, char *);
+
+			if (p != NULL)
+				printf("%s", p);
+			if (x < n - 1)
+				printf("%s", separator);
+			if (p == NULL)
+				printf("%p", p);
+		}
+		printf("\n");
+	}
+
+	va_end(string);
+}
