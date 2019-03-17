b944a1e660120ea5853719bbba2974110211a60a
Alejo-Rey <771@holbertonschool.com>
2019-03-17 22:03:10 +0000

    [feature] funtion to print anything

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -1,11 +1,51 @@
 #include "variadic_functions.h"
 
 /**
- * print_all -
+ * print_all - function that prints anything.
+ * @format: type of yhe element to print
  *
- * Return: 
+ * Return: Nan
  */
 void print_all(const char * const format, ...)
 {
+	va_list all;
+	char *p;
+	int x = 0, y = 0;
 
+	va_start(all, format);
+
+	while (format[x])
+		x++;
+	while (format[y])
+	{
+		switch (format[y])
+		{
+		case 'c':
+			printf("%c", va_arg(all, int));
+			break;
+		case 'i':
+			printf("%i", va_arg(all, int));
+			break;
+		case 's':
+			p = va_arg(all, char*);
+			if (p == NULL)
+			{
+				printf("%p", p);
+				break;
+			}
+			printf("%s", p);
+			break;
+		case 'f':
+			printf("%f", va_arg(all, double));
+			break;
+		default:
+			y++;
+			continue;
+		}
+		if (y < x - 1)
+			printf(", ");
+		y++;
+	}
+	printf("\n");
+	va_end(all);
 }
