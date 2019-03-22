746080e603cbc8499b52db557e60440a2c0ace6f
Alejo-Rey <771@holbertonschool.com>
2019-03-22 03:24:29 +0000

    linked list

--- 0x11-singly_linked_lists/101-hello_holberton.asm
+++ 0x11-singly_linked_lists/101-hello_holberton.asm
@@ -2,19 +2,19 @@ SYS_EXIT  equ 1
 SYS_WRITE equ 4
 STDIN     equ 0
 STDOUT    equ 1
-section	 .text
+section  .text
 	global main
 
 main:
 	mov eax, SYS_WRITE
 	mov ebx, STDOUT
-	mov ecx, msg1
-	mov edx, len1
-	int 0x80
+        mov ecx, msg1
+        mov edx, len1
+        int 0x80
 
 	mov eax,SYS_EXIT
 	int 0x80
 
-section	 .data
-msg1 db	'Hello, Holberton',0xA,0xD
+section  .data
+msg1 db 'Hello, Holberton',10
 len1 equ $ - msg1
