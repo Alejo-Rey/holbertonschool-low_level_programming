39795b43ca368a6bd54ab8bcd55da7ea00542a47
Alejo-Rey <771@holbertonschool.com>
2019-03-22 03:47:15 +0000

    linked list

--- 0x11-singly_linked_lists/101-hello_holberton.asm
+++ 0x11-singly_linked_lists/101-hello_holberton.asm
@@ -1,20 +1,16 @@
-SYS_EXIT  equ 1
-SYS_WRITE equ 4
-STDIN     equ 0
-STDOUT    equ 1
-section  .text
-	global main
-
+section .data
+	msg db "Hello, Holberton", 0x0a ;Mensaje
+	len equ $ - msg ;Tamaño del mensaje
+section .text
+	global main ;Necesitamos exportar 'main' a ld, para que lo reconozca
+                                         ;como el punto de entrada del programa.
 main:
-	mov eax, SYS_WRITE
-	mov ebx, STDOUT
-        mov ecx, msg1
-        mov edx, len1
-        int 0x80
-
-	mov eax,SYS_EXIT
-	int 0x80
-
-section  .data
-msg1 db 'Hello, Holberton',10
-len1 equ $ - msg1
+;Escribimos el saludo a stdout.
+	mov eax, 4 ;Número de la llamada al sistema (sys_write).
+	mov ebx, 1 ;Primer argumento: file handle (stdout).
+	mov ecx, msg ;Segundo argumento: puntero a la cadena a escribir.
+	mov edx, len ;Tercer argumento: tamaño de la cadena.
+	int 0x80     ;Llama al Kernel.
+	mov eax, 1  ;Número de la llamada al sistema (sys_exit).
+	xor ebx, ebx ;Argumento de la llamada: código de salida (0: ejecución exitosa).
+	int 0x80 ;Llama al Kernel.
