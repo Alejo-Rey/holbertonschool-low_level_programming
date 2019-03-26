a73774fe82faf676f54909df41dee422a7b679cf
Alejo-Rey <771@holbertonschool.com>
2019-03-26 23:14:48 +0000

    [new] function to free the first node and return the node free

--- 0x12-more_singly_linked_lists/6-pop_listint.c
+++ 0x12-more_singly_linked_lists/6-pop_listint.c
@@ -7,5 +7,17 @@
  */
 int pop_listint(listint_t **head)
 {
+	int x;
+	listint_t *tmp;
+
+	if (*head == NULL)
+		return (0);
+
+	tmp = *head;
+	x = (*head)->n;
+	*head = tmp->next;
+	free(tmp);
+
+	return (x);
 
 }
