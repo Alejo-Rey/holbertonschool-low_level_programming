3598eee8f8dad4c95d93ba5f6070abdb504e2358
Alejo-Rey <771@holbertonschool.com>
2019-03-27 00:31:17 +0000

    [feature] function to search a specific node

--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -1,11 +1,21 @@
 #include "lists.h"
 
 /**
- * get_nodeint_at_index -
- *
- * Return: 
+ * get_nodeint_at_index - function to find a node in a list
+ * @head: the head of the list
+ * @index: the number of the node to search
+ * Return: the searched node
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
+	unsigned int x = 0;
 
+	while (x < index)
+	{
+		if (head == NULL)
+			return (NULL);
+		head = head->next;
+		x++;
+	}
+		return (head);
 }
