a03826d0a86af6eca03e580933f8f9d1155d71b7
Alejo-Rey <771@holbertonschool.com>
2019-03-27 00:39:05 +0000

    [new] function to sum the data of the nodes

--- 0x12-more_singly_linked_lists/8-sum_listint.c
+++ 0x12-more_singly_linked_lists/8-sum_listint.c
@@ -1,11 +1,21 @@
 #include "lists.h"
 
 /**
- * sum_listint -
- *
- * Return: 
+ * sum_listint - function to sum the data of the list
+ * @head: the headerof the list
+ * Return: x, the sum
  */
 int sum_listint(listint_t *head)
 {
+	unsigned int x = 0;
 
+	if (head == NULL)
+		return (0);
+	while (head)
+	{
+		x += head->n;
+		head = head->next;
+	}
+
+	return (x);
 }
