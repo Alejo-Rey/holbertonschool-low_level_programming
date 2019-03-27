6e4c4f5eb350a79c234c1ccc0f3b8f869d464798
Alejo-Rey <771@holbertonschool.com>
2019-03-27 01:06:19 +0000

    [try]

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,11 +1,23 @@
 #include "lists.h"
 
+
 /**
- * insert_nodeint_at_index -
- *
+ * insert_nodeint_at_index - 
+ * @head: the header of the list
+ * @idx:
+ * @n:
  * Return: 
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
+	unsigned int x = 0;
+	listint_t *temp;
+
+	while (x < idx)
+	{
+		head = head->next;
+		x++;
+	}
+	temp-
 
 }
