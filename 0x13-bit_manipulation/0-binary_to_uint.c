d8fa9af6f541c033745283443c899901487af4ba
Alejo-Rey <771@holbertonschool.com>
2019-03-28 22:54:05 +0000

    [fix] the return NULL and betty

--- 0x13-bit_manipulation/0-binary_to_uint.c
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -23,14 +23,17 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int y = 0, sum = 0;
 	int x = 0;
 
+	if (!b)
+		return (0);
 	while (b[x])
 		x++;
 	x--;
 	for ( ; x >= 0; x--)
 	{
 		if (b[x] != 48 && b[x] != 49)
-		       return (0);	
-		sum += (int) (b[x] - 48) * (_pow(2,y));
+			return (0);
+
+		sum += (int) (b[x] - 48) * (_pow(2, y));
 		y++;
 	}
 	return (sum);
