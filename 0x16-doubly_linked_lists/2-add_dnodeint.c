fc79f20352245dac1ba07bfcea969e33c84064a9
Alejo-Rey <771@holbertonschool.com>
2019-05-05 18:03:49 +0000

    add a node to the begening od the list

--- 0x16-doubly_linked_lists/2-add_dnodeint.c
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -1,11 +1,21 @@
 #include "lists.h"
 
 /**
- * add_dnodeint -
- *
- * Return: 
+ * add_dnodeint - funtion to add a node in the beginnig of the list
+ * @head: the head of the list
+ * @n: the date of the node
+ * Return: return the address of the new node
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
+	dlistint_t *t;
 
+	t = malloc(sizeof(dlistint_t));
+	if (!t)
+		return (NULL);
+	t->n = n;
+	t->next = *head;
+	t->prev = NULL;
+	*head = t;
+	return (t);
 }
