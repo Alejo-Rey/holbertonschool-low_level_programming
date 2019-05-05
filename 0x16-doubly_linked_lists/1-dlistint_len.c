ff919a2731f24eaa2b6e4ac09e70f89347ea3316
Alejo-Rey <771@holbertonschool.com>
2019-05-05 17:09:14 +0000

    return the number of the nodes

--- 0x16-doubly_linked_lists/1-dlistint_len.c
+++ 0x16-doubly_linked_lists/1-dlistint_len.c
@@ -1,11 +1,19 @@
 #include "lists.h"
 
 /**
- * dlistint_len -
- *
- * Return: 
+ * dlistint_len - funtion to return the number of the nodes
+ * @h: the header of the list
+ * Return: the number of the nodes
  */
 size_t dlistint_len(const dlistint_t *h)
 {
+	int x = 0;
 
+	if (!h)
+		return (0);
+	for ( ; h; x++)
+	{
+		h = h->next;
+	}
+	return (x);
 }
