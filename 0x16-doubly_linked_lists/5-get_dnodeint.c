a52bfc2b6eee2d3d875c062a886942bfc802a9b6
Alejo-Rey <771@holbertonschool.com>
2019-05-06 00:47:45 +0000

    find a node from the index

--- 0x16-doubly_linked_lists/5-get_dnodeint.c
+++ 0x16-doubly_linked_lists/5-get_dnodeint.c
@@ -1,11 +1,25 @@
 #include "lists.h"
 
 /**
- * get_dnodeint_at_index -
- *
- * Return: 
+ * get_dnodeint_at_index - get a node of specific index
+ * @head: head of the list
+ * @index: the node to return
+ * Return: the node finded
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
+	dlistint_t *find;
+	unsigned int x = 0;
 
+	find = head;
+	if (!find)
+		return (NULL);
+	while(find)
+	{
+		if (x == index)
+			return (find);
+		find = find->next;
+		x++;
+	}
+	return (NULL);
 }
