9340ce3812e0e2c7c3b6de73321a38a0a6ebe03e
Alejo-Rey <771@holbertonschool.com>
2019-05-12 03:35:48 +0000

    insert a node in the index give it

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,11 +1,33 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index -
- *
- * Return: 
+ * insert_dnodeint_at_index - function to inser a node in the index position
+ * @h: the head node
+ * @idx: the index to insert he node
+ * @n: the numbert to insert
+ * Return: the addres of the new node
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
+	dlistint_t *new, *prev, *next;
+	unsigned int x;
 
+	new = malloc(sizeof(dlistint_t));
+
+	if (!*h || !new)
+		return NULL;
+	prev = *h;
+	new->n = n;
+
+	for (x = 1; x < idx; x++)
+		prev = prev->next;
+
+	next = prev->next;
+	new->prev = prev;
+	if (prev != NULL)
+		prev->next = new;
+	new->next = next;
+	if (next)
+		next->prev = new;
+	return (new);
 }
