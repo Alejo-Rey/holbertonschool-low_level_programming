47a14fa7605287ddc3df2625bae8219042005240
Alejo-Rey <771@holbertonschool.com>
2019-05-12 04:07:36 +0000

    insert a node in the index

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -25,7 +25,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		*h = new;
 		return (new);
 	}
-	for (x = 1; x < idx && prev; x++)
+	for (x = 1; x < idx && prev->next; x++)
 		prev = prev->next;
 
 	next = prev->next;
