f88668a53907a0d8dfb7ff25a65520f89e67dcb5
Alejo-rey <771@holbertonschool.com>
2019-06-01 03:40:56 +0000

    headers, prototypes and structs

--- 0x19-hash_tables/hash_tables.h
+++ 0x19-hash_tables/hash_tables.h
@@ -42,5 +42,6 @@ char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
 hash_node_t *add_node(hash_node_t **head, const char *str, const char *key);
+size_t print_listint(const hash_node_t *h);
 
 #endif /* HASH_TABLES_H */
