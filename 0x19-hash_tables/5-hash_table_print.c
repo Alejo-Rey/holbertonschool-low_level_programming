81557ec36930d859ac0f89c3250221d64c10da77
Alejo-rey <771@holbertonschool.com>
2019-06-01 00:04:06 +0000

    function to print a hash table

--- 0x19-hash_tables/5-hash_table_print.c
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -1,11 +1,27 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_print -
- *
- * Return: 
+ * hash_table_print - function to print a hash table
+ * @ht: the has table
+ * Return: Not return
  */
 void hash_table_print(const hash_table_t *ht)
 {
+	unsigned long int x, y = 0;
+	hash_node_t *temp;
 
+	printf("{");
+	for (x = 0; x < ht->size; x++)
+	{
+		if (ht->array[x])
+		{
+			temp = ht->array[x];
+			if (y == 1)
+				printf(", ");
+			printf("'%s': '%s'", temp->key, temp->value);
+			free(temp);
+			y = 1;
+		}
+	}
+	printf("}\n");
 }
